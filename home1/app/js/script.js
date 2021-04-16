1f83b39e9620b83cba4598e8b80a49ef2f96c6ee
Dmitriy Gryaznykh <gry-dmitrij@yandex.ru>
2021-04-16 13:06:42 +0300

    - Сделано задание 1

--- home1/app/js/script.js
+++ home1/app/js/script.js
@@ -5,13 +5,21 @@ const goods = [
 	{ title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price) => {
-	return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = ({title = 'Товар в пути', price= 0}) => {
+	return `<div class="goods-item"><h3>${title}</h3>
+		<img src="https://picsum.photos/200/300?random=${Math.floor(Math.random() * 1000)}">
+		<p>Цена: ${price}</p></div>`;
 };
 
-const renderGoodsList = (list) => {
-	let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-	document.querySelector('.goods-list').innerHTML = goodsList;
+const renderGoodsList = (list = [{}]) => {
+	let goodsList = list.map(item => renderGoodsItem(item));
+	document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
-renderGoodsList(goods);
+// при загрузке добавим слушателя кнопке
+document.addEventListener('DOMContentLoaded', () => {
+	const button = document.querySelector('.cart-button');
+	button.addEventListener('click', function (event){
+		renderGoodsList(goods);
+	});
+});
